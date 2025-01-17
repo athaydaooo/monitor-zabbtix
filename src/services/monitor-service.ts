ca2927b498e75c5f4fea75e51e4469281f9a1be6
Lucas Athayde <athaydaooo@gmail.com>
2025-01-17 16:53:46 -0300

    init

---
+++ src/services/monitor-service.ts
@@ -0,0 +1,46 @@
+import { MikroTikClient } from "../clients/mikrotik-client";
+import { ZabbixClient } from "../clients/zabbix-client";
+import { ZabbixSenderService } from "./zabbix-sender-service";
+
+
+export class MonitorService {
+  private zabbixClient: ZabbixClient;
+  private mikrotikClient: MikroTikClient;
+  private zabbixSender: ZabbixSenderService;
+
+  constructor(
+    zabbixApiUrl: string,
+    mikrotikUsername: string,
+    mikrotikPassword: string,
+    zabbixServer: string,
+  ) {
+    this.zabbixClient = new ZabbixClient(zabbixApiUrl);
+    this.mikrotikClient = new MikroTikClient(mikrotikUsername, mikrotikPassword);
+    this.zabbixSender = new ZabbixSenderService(zabbixServer);
+  }
+
+  async loadBalanceMonitor(): Promise<void> {
+    const dstAddress = "google.com"
+    try {
+      const hosts = await this.zabbixClient.getHosts();
+
+      for (const host of hosts) {
+        const { ip, name } = host;
+
+        try {
+          const interface1Ping = await this.mikrotikClient.testPing(ip, dstAddress,'ether1');
+          const interface2Ping = await this.mikrotikClient.testPing(ip, dstAddress, 'ether2');
+
+          await this.zabbixSender.sendData(name, 'mikrotik.interface1.ping', interface1Ping);
+          await this.zabbixSender.sendData(name, 'mikrotik.interface2.ping', interface2Ping);
+        } catch (error) {
+          console.log(`Erro ao processar MikroTik ${ip}:`, error);
+        }
+      }
+
+      await this.zabbixSender.sendAll();
+    } catch (error) {
+        console.log('Erro no processo de monitoramento:', error);
+    }
+  }
+}
