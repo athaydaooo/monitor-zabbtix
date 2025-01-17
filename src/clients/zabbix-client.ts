dae11adfcda7af00595a50f2242d4a122a76eae2
Lucas Athayde <athaydaooo@gmail.com>
2025-01-17 18:01:50 -0300

    feature/zabbix client

--- src/clients/zabbix-client.ts
+++ src/clients/zabbix-client.ts
@@ -1,22 +1,73 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+import Host from "../entities/host";
+
+interface AuthorizeResponseData {
+  jsonrpc: string;
+  result: string;
+  id: number;
+}
+
+interface GetHostsResponseData {
+  jsonrpc: string;
+  result: Host[];
+  id: number;
+}
 
 export class ZabbixClient {
-  private zabbixApiUrl: string;
+  private client: AxiosInstance;
+  private authKey: string | null;
 
   constructor(zabbixApiUrl: string) {
-    this.zabbixApiUrl = zabbixApiUrl;
+    this.authKey = null;
+    this.client = axios.create({
+      baseURL: zabbixApiUrl,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
-  async getHosts(): Promise<{ ip: string; name: string }[]> {
-    try {
-      const response = await axios.get(`${this.zabbixApiUrl}/hosts`);
-      return response.data.map((host: any) => ({
-        ip: host.ip,
-        name: host.name,
-      }));
-    } catch (error) {
-      console.log("Erro ao consultar hosts no Zabbix:", error);
-      throw error;
-    }
+  async authorize(username: string, password: string): Promise<void> {
+    const auth = await this.client
+      .post("", {
+        body: {
+          jsonrpc: "2.0",
+          method: "user.login",
+          params: {
+            user: username,
+            password: password,
+          },
+          id: 1,
+          auth: null,
+        },
+      })
+      .then((response) => response)
+      .catch((err) => err.response);
+
+    if (auth.status !== 200) throw Error("autenticacao falhou");
+
+    this.authKey = auth.data.result;
+  }
+
+  async getHosts(groupId?: string): Promise<Host[]> {
+    const hosts = await this.client
+      .post("", {
+        body: {
+          jsonrpc: "2.0",
+          method: "host.get",
+          params: {
+            output: ["hostid", "host"],
+            groupids: groupId,
+          },
+          id: 2,
+          auth: this.authKey,
+        },
+      })
+      .then((response) => response.data as AuthorizeResponseData)
+      .catch((err) => err.response);
+
+    if (hosts.status !== 200) return [];
+
+    return hosts.data.result;
   }
 }
