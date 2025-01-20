7e21dd1e3ae3d0e8890aeecd79eef577f56c633c
Lucas Athayde <athaydaooo@gmail.com>
2025-01-20 12:58:08 -0300

    refactor/ filename fix

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,25 @@
 import winston from "winston";
 
 const logger = winston.createLogger({
-  level: "error", // Define o nível mínimo de log
+  level: "info", // Define o nível mínimo de log como "info"
   format: winston.format.combine(
     winston.format.timestamp(), // Adiciona um timestamp ao log
     winston.format.json() // Formata o log como JSON
   ),
   transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }), // Log de erros em arquivo
-    new winston.transports.Console(), // Log no console
+    // Logs de erro em um arquivo separado
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+
+    // Logs informativos em outro arquivo
+    new winston.transports.File({ filename: "info.log", level: "info" }),
+
+    // Logs no console (opcional)
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(), // Adiciona cores ao console
+        winston.format.simple() // Formato simples para o console
+      ),
+    }),
   ],
 });
 
