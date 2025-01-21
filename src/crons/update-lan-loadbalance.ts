d749d826400faae09ca110692e7048257254123e
Lucas Athayde <athaydaooo@gmail.com>
2025-01-21 14:05:07 -0300

    refactor/Interface import fix

--- src/crons/update-lan-loadbalance.ts
+++ src/crons/update-lan-loadbalance.ts
@@ -1,5 +1,5 @@
 import { ZabbixApiClient } from "@clients/zabbix-api/zabbix-api-client";
-import { ZabbixSenderClient } from "@clients/zabbix-sender/zabbix-sender-client";
+import { IZabbixSenderClient } from "@clients/zabbix-sender/zabbix-sender-client";
 import config from "@config/index";
 import { AppError } from "@errors/index";
 import { MonitorService } from "@services/monitor-service";
@@ -7,7 +7,7 @@ import logger from "@utils/logger";
 
 async function main() {
   try {
-    const zabbixSender = new ZabbixSenderClient(
+    const zabbixSender = new IZabbixSenderClient(
       config.zabbixServer,
       config.zabbixServerPort
     );
@@ -19,7 +19,7 @@ async function main() {
 
     const monitorService = new MonitorService(zabbixClient, zabbixSender);
 
-    await monitorService.checkAllLoadBalances();
+    await monitorService.updateLanLoadbalance();
   } catch (error) {
     if (error instanceof AppError) logger.error(error.message, error);
 
