0c36f2577f1f314116574ff1edf995427ee39fe6
Lucas Athayde <athaydaooo@gmail.com>
2025-02-18 11:05:47 -0300

    run deploy

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import updateAllLans from "jobs/update-all-lan";
 import cron from "node-cron";
 
 cron.schedule("* * * * *", async () => {
-  logger.info("Running updateAllLans!!");
+  logger.info("Running updateAllLans");
   await updateAllLans();
 });
 
 cron.schedule("* * * * *", async () => {
   setTimeout(async () => {
-    logger.info("Running updateAllLans!!");
+    logger.info("Running updateAllLans");
     await updateAllLans();
   }, 30000);
 });
