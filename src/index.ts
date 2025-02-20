88a91aafba039afb962757235957d3d1fdd136f5
Lucas Athayde <athaydaooo@gmail.com>
2025-02-20 15:53:43 -0300

    fix crons

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,20 @@ import updateAllL2TPServer from "jobs/update-all-l2tp-server";
 import updateAllLans from "jobs/update-all-lan";
 import cron from "node-cron";
 
-//LAN CRON
 cron.schedule("* * * * *", async () => {
   logger.info("Running updateAllLans");
   await updateAllLans();
+
+  logger.info("Running updateAllL2TPServer");
+  await updateAllL2TPServer();
 });
 
 cron.schedule("* * * * *", async () => {
   setTimeout(async () => {
     logger.info("Running updateAllLans");
     await updateAllLans();
+
+    logger.info("Running updateAllL2TPServer");
+    await updateAllL2TPServer();
   }, 30000);
-});
-
-//L2TP SERVER CRON
-cron.schedule("* * * * *", async () => {
-  setTimeout(async () => {
-    logger.info("Running updateAllL2TPServer");
-    await updateAllL2TPServer();
-  }, 15000);
-});
-
-cron.schedule("* * * * *", async () => {
-  setTimeout(async () => {
-    logger.info("Running updateAllL2TPServer");
-    await updateAllL2TPServer();
-  }, 45000);
 });
